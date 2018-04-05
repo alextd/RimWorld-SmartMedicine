a295e1fa492365eb16e65a217916eae96c2f261e
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-04 18:50:35 -0700

    Include inventory in count for low medicine warning

---
+++ Source/StockUp/LowMedicineWarning.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace SmartMedicine.StockUp
+{
+	[HarmonyPatch(typeof(Alert_LowMedicine), "MedicineCount")]
+	static class LowMedicineWarning
+	{
+		//private int MedicineCount(Map map)
+		static void Postfix(Map map, ref int __result)
+		{
+			int invCount = 0;
+
+			foreach (Pawn pawn in map.mapPawns.FreeColonistsSpawned)
+				foreach (Thing thing in pawn.inventory.innerContainer)
+					if (ThingRequestGroup.Medicine.Includes(thing.def))
+						invCount += thing.stackCount;
+
+			__result += invCount;
+		}
+	}
+}
