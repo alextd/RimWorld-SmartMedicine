8a20ae7a1664f789a55dedf994404fb1cd3f2dc1
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-03 19:05:30 -0700

    Patch WhileYoureUp since people are dying over it

---
+++ Source/FixWhileYoureUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace SmartMedicine
+{
+	[StaticConstructorOnStartup]
+	static class FixWhileYoureUp
+	{
+		static FixWhileYoureUp()
+		{
+			if(AccessTools.TypeByName("WhileYoureUp.Utils") is Type whileYoureUpUtils)
+			{
+				if(AccessTools.Method(whileYoureUpUtils, "MyDistance") is MethodInfo patchThis)
+				{
+					HarmonyInstance.Create("uuugggg.rimworld.SmartMedicine.main").Patch(patchThis,
+						new HarmonyMethod(typeof(FixWhileYoureUp), nameof(Prefix)));
+				}
+			}
+		}
+
+		//public static int MyDistance(IntVec3 a, LocalTargetInfo b, Map m, TraverseParms t)
+		public static void Prefix(ref LocalTargetInfo b)
+		{
+			if (!b.Thing?.Spawned ?? false)
+				b = b.Thing.PositionHeld;
+		}
+	}
+}
