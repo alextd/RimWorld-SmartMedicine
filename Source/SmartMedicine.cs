3503af1aa903f24bdb6716ce3ee10b6f4b986615
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2020-08-30 13:21:52 -0700

    removing DefOf Warning suppression. Seems to not be a problem anymore.

--- Source/SmartMedicine.cs
+++ Source/SmartMedicine.cs
@@ -20,18 +20,7 @@ namespace SmartMedicine
 
 			Harmony harmony = new Harmony("uuugggg.rimworld.SmartMedicine.main");
 
-			//Turn off DefOf warning since harmony patches trigger it.
-			MethodInfo DefOfHelperInfo = AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor");
-			if (!harmony.GetPatchedMethods().Contains(DefOfHelperInfo))
-				harmony.Patch(DefOfHelperInfo, new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
-
 			harmony.PatchAll();
-		}
-
-		public static bool EnsureInitializedInCtorPrefix()
-		{
-			//No need to display this warning.
-			return false;
 		}
 
 		public override void DoSettingsWindowContents(Rect inRect)
